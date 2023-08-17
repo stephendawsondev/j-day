b472e0f4e4250508ae2429a868c7cda42c6a331f
Stephen Dawson <stephendawson@protonmail.com>
2023-08-17 20:18:36 +0100

    add initial user function and output to canvas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 import generateScenes from "./scenes.js";
+import spawnEnemy from "./enemy.js";
 
 // initialize kaboom context
 kaboom({
@@ -6,35 +7,32 @@ kaboom({
   width: 800,
   height: 600,
   canvas: document.getElementById("game-canvas"),
-  fullscreen: true,
   scale: 1,
 });
 
 /* Kyle added sprite import */
 
-loadRoot("../../public/sprites/");
-loadSprite("background_tile1","background_main_tile.png");
+loadRoot("sprites/");
+loadSprite("background-tile", "background-main-tile.png");
 
-loadRoot("../../public/sounds/");
-loadSound("death_sound","death.wav");
-loadSound("hurt_sound","hit_hurt.wav");
-loadSound("life_pickup","life_pickup.wav");
-loadSound("powerup_sound","powerup.wav");
-loadSound("game_over", "game_over.wav");
-loadSound("intro_music", "intro.wav");
-loadSound("main_music", "main.wav");
-loadSound("menu_select", "menu_select.wav");
+loadRoot("sounds/");
+loadSound("death_sound", "death.wav");
+loadSound("hurt_sound", "hit_hurt.wav");
+loadSound("life_pickup", "life_pickup.wav");
+loadSound("powerup_sound", "powerup.wav");
 
+loadRoot("scene_background_images");
+loadSprite("game_over", "game_over_background.png");
 
 loadRoot("../../public/scene_background_images/");
 loadSprite("instructions-page", "instructions.png");
-loadSprite("game_over","game_over_background.png");
-loadSprite("welcome_page", "welcome_img.png")
 
+spawnEnemy(300, 300);
 
 generateScenes();
 
 go("welcome");
 
+spawnEnemy(300, 300);
 // canvas focus enables user keyboard input register
 canvas.focus();
