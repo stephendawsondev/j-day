b7088fbf47b172c65d7170df9006750e0c7e3206
KTC96 <kyleclow96@gmail.com>
2023-08-18 14:26:10 +0000

    Add intro, main music and menu sound, and import into main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,10 @@ loadSound("death_sound","death.wav");
 loadSound("hurt_sound","hit_hurt.wav");
 loadSound("life_pickup","life_pickup.wav");
 loadSound("powerup_sound","powerup.wav");
-loadSound("game_over", "game_over.wav")
+loadSound("game_over", "game_over.wav");
+loadSound("intro_music", "intro.wav");
+loadSound("main_music", "main.wav");
+loadSound("menu_select", "menu_select.wav");
 
 
 loadRoot("../../public/scene_background_images/");
