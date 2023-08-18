c2e0ba3bc3cf8cd06019cfc8e94a50781077f25b
KTC96 <kyleclow96@gmail.com>
2023-08-18 13:03:47 +0000

    Add scene setup to scenes.js including tile creation, links to instructions, game over screen, display score, and play again

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,6 +20,8 @@ loadSound("death_sound","death.wav");
 loadSound("hurt_sound","hit_hurt.wav");
 loadSound("life_pickup","life_pickup.wav");
 loadSound("powerup_sound","powerup.wav");
+loadSound("game_over", "game_over.wav")
+
 
 loadRoot("../../public/scene_background_images/");
 loadSprite("instructions-page", "instructions.png");
--- assets/js/scenes.js
+++ assets/js/scenes.js
@@ -1,49 +1,150 @@
-// add background tiles
-const generateFloorTiles = () => {
-    let positionX = 0;
-    let positionY = 0;
-    for (let i = 0; i < width(); i++) {
-      if (positionX > width()) {
-        positionX = 0;
-        positionY += 32;
-      }
-      add([
-        sprite("background_main_tile"),
-        pos(positionX, positionY),
-        scale(1),
-        layer("bg"),
-      ]);
-
-      positionX += 32;
-    }
-  };
-
 /**
  * Generates the scenes for the game - called by go("sceneName")
  */
- const generateScenes = () => {    
-    // add instructions
-    scene("instructions", () => {
-      const instructionsBackground = add([
-        sprite("instructions-page"),
-        pos(0, 0),
-        origin("topleft"),
-        scale(1),
-      ]);
-  
-      /**add([
-        text("Go back"),
-        pos(600, 550),
-        scale(0.5),
-        origin("left"),
-        area(),
-        "back",
-      ]);
-  
-      onClick("back", () => {
-        go("instructions");
-      });**/
+const generateScenes = () => {
+  // add welcome screen
+  scene("welcome", () => {
+    const welcomeBackground = add([
+      sprite("welcome"),
+      pos(0, 0),
+      origin("topleft"),
+      scale(1),
+    ]);
+    const startText = add([
+      text("Start Game"),
+      color(YELLOW),
+      pos(width() / 4, height() / 2),
+      scale(0.5),
+      origin("center"),
+      area(),
+      "start-text",
+    ]);
+    const instructionsText = add([
+      text("How to play"),
+      color(YELLOW),
+      pos(width() / 4, height() / 2 + 100),
+      scale(0.5),
+      origin("center"),
+      area(),
+      "instructions-text",
+    ]);
+
+    onClick("start-text", () => {
+      go("game", {score: 0, livesLeft: 3 });
     });
-  };
+
+    onClick("instructions-text", () => {
+      go("instructions");
+    });
+
+    onKeyDown("enter", () => {
+      go("game", { score: 0, livesLeft: 3 });
+    });
+  });
+
+  // add instructions screen
+  scene("instructions", () => {
+    const instructionsBackground = add([
+      sprite("instructions"),
+      pos(0, 0),
+      origin("topleft"),
+      scale(1),
+    ]);
+
+    add([
+      text("Go back"),
+      pos(600, 550),
+      color(YELLOW),
+      scale(0.5),
+      origin("left"),
+      area(),
+      "back",
+    ]);
+
+    onClick("back", () => {
+      go("welcome");
+    });
+  });
+
+  // add the game scene
+  scene("game", ({ timeLeft, score, livesLeft }) => {
+    layers(["bg", "game", "ui"], "game");
+
+    // add background tiles
+    const generateFloorTiles = () => {
+      let positionX = 0;
+      let positionY = 0;
+      for (let i = 0; i < width(); i++) {
+        if (positionX > width()) {
+          positionX = 0;
+          positionY += 34;
+        }
+        add([
+          sprite("background-floor-tile"),
+          pos(positionX, positionY),
+          scale(1),
+          layer("bg"),
+        ]);
+
+        positionX += 34;
+      }
+    };
+
+    generateFloorTiles();
+   
+    // display score
+    add([
+      text(`Score:${score}`),
+      pos(width() * 0.01, 0),
+      layer("ui"),
+      scale(0.4),
+    ]);
+
+    // Display lives remaining
+    add([
+      text(`Lives left:${livesLeft}`),
+      pos(width() * 0.3, 0),
+      layer("ui"),
+      scale(0.4),
+    ]);
+  });
+
+  // add the game over scene
+  scene("game_over", (score) => {
+    layers(["bg", "game", "ui"], "game");
+    const gameOverBackground = add([
+      sprite("game_over_background"),
+      pos(0, 0),
+      origin("topleft"),
+      scale(1),
+      layer("bg"),
+      kaboom.audio.play("game_over", { loop: false, volume: 0.5 })
+
+    ]);
+
+    // display score
+    add([
+      text(`Your Score:${score}`),
+      pos(width() * 0.25, height() * 0.3),
+      color(YELLOW),
+      layer("ui"),
+      scale(0.8),
+    ]);
+
+    const startText = add([
+      text("Play again"),
+      color(YELLOW),
+      pos(width() / 2, height() - height() / 10),
+      scale(0.5),
+      origin("center"),
+      area(),
+      "play-again-text",
+    ]);
+
+    onClick("play-again-text", () => {
+      go("game", {score: 0, livesLeft: 3 });
+    });
+  });
+};
 
 export default generateScenes;
