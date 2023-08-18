71b0c80ad4113c2e71cfcbdc6bc0c5c7b91909f0
Koko <pointmisser@gmail.com>
2023-08-18 23:19:39 +0100

    Update movement functionality.
    Change keys from arrows to WASD.
    
    Restrict player movement to the canvas.
    
    Increase player speed to 200.
    
    Update onKeyDown to render different sprites depending on previous direction across the x-axis.

--- assets/js/player.js
+++ assets/js/player.js
@@ -1,35 +1,50 @@
-export const spawnPlayer = () => {
+const spawnPlayer = () => {
   const player = add([
     sprite("sarah_l"),
     area(),
     scale(1.5),
-    pos(0, 0),
+    pos(0, 20),
     "sarah",
   ]);
 
+  const playerHeight = 90;
+  const playerWidth = 96;
   // set movement speed
-  let speed = 150;
+  let speed = 200;
 
-  // Define keyboard keys for the moves
-  onKeyDown("left", () => {
-    player.use(sprite("sarah_l"));
-    player.move(-speed, 0);
+  // variable to store player direction to select
+  // correct avatar when moving down
+  let facingR = false;
+
+  // Define keyboard keys for the player movements
+  onKeyDown("a", () => {
+    if (player.pos.x > 0) {
+      player.use(sprite("sarah_l"));
+      player.move(-speed, 0);
+      facingR = false
+    }
   });
 
-  onKeyDown("right", () => {
-    player.use(sprite("sarah_r"));
-    player.move(speed, 0);
+  onKeyDown("d", () => {
+    if (player.pos.x <= width() - playerWidth) {
+      player.use(sprite("sarah_r"));
+      player.move(speed, 0);
+      facingR = true;
+    }
   });
 
-  onKeyDown("up", () => {
-    player.use(sprite("sarah_b"));
-    player.move(0, -speed);
+  onKeyDown("w", () => {
+    if (player.pos.y > 20) {
+      player.use(sprite("sarah_b"));
+      player.move(0, -speed);
+    }
   });
 
-  onKeyDown("down", () => {
-    player.move(0, speed);
-    player.use(sprite("sarah_l"));
+  onKeyDown("s", () => {
+    if (player.pos.y <= height() - playerHeight) player.move(0, speed);
+    facingR ? player.use(sprite("sarah_r")) : player.use(sprite("sarah_l"));
   });
-
   return player;
 };
+
+export { spawnPlayer };
